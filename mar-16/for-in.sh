30111969bf9c87350ef50aface7578bd3e6c5408
Hans-Peter Bergner <hapebe@gmx.de>
2018-03-16 13:24:16 +0100

    TGIF!

--- mar-16/for-in.sh
+++ mar-16/for-in.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+echo "Namen:"
 names='Horst Dieter Ina Luzie'
 for i in $names
 do
 	echo $i
+done
+echo
+echo "Dateien in folder:"
+for filename in folder/*.txt ; do
+	# avoid using the pattern itself, it there are no matches:
+	[ -e $filename ] || continue
+
+	# payload:
+	echo $filename
 done
