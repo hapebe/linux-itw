3330fd7201c73104da482957e678db07e991058d
Hans-Peter Bergner <hapebe@gmx.de>
2018-04-19 14:11:03 +0200

    find-in-files.sh extended

--- mar-19/find-in-files.sh
+++ mar-19/find-in-files.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 if [ $# -lt 1 ] ; then
-	echo "Usage: $0 <search string>"
+	echo "Usage: $0 [directory] <search string>"
 	exit 1
 fi
-find / -type f \
+if [ $# -eq 1 ] ; then
+	TOPDIR=$(pwd -P)
+	SEARCH=$1
+fi
+if [ $# -eq 2 ] ; then
+	TOPDIR=$1
+	SEARCH=$2
+fi
+if [ ! -d $TOPDIR ] ; then
+	echo "Error: $TOPDIR is not directory or cannot be accessed."
+	exit 1
+fi
+echo "Searching for \"$SEARCH\" in directory $TOPDIR ..."
+find $TOPDIR -type f \
 ! -path "/dev/*" \
 ! -path "/proc/*" \
 ! -path "/sys/*" \
@@ -13,5 +26,5 @@ find / -type f \
 ! -path "*/.cache/*" \
 ! -name "*.iso" \
 ! -name "*.vdi" \
--exec ./grep-file.sh $1 {} ';' \
+-exec ./grep-file.sh "$SEARCH" {} ';' \
 2>/dev/null
--- mar-19/grep-file.sh
+++ mar-19/grep-file.sh
@@ -2,8 +2,8 @@
 # expects two parameters:
 # $1 = <pattern>
 # $2 = <filename>
-if grep -q $1 "$2" ; then
+if grep -q "$1" "$2" ; then
 	echo "#### $2"
 	# this adds line numbers to the file before searching (and printing matches)
-	cat -n $2 | grep $1
+	cat -n "$2" | grep "$1"
 fi
