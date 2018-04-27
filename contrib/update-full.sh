7a25b41fc98065b22096b8ea606bdba1578c8991
Hans-Peter Bergner <hapebe@gmx.de>
2018-04-27 18:25:17 +0200

    added test for root privileges

--- contrib/update-full.sh
+++ contrib/update-full.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#
-# run this as root!
-#
+if [ $(id -u) -ne 0 ] ; then
+	echo "This script must be run as root.";
+	exit 1
+fi
 apt-get update; apt-get -y dist-upgrade; apt-get autoremove; apt-get autoclean
