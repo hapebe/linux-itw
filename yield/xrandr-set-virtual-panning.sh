21389a92785fce2a0760c0f2fb07b96ac199b1c0
Hans-Peter Bergner <hapebe@gmx.de>
2018-08-01 10:57:53 +0200

    enable virtual panning (GNOME / Ubuntu 18)

---
+++ yield/xrandr-set-virtual-panning.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ $# -eq 0 ] ; then
+	echo "usage:"
+	echo "$0 <virtual resolution>"
+	printf "\te.g. $0 2560x1440\n"
+	echo "or:"
+	echo "$0 off"
+	echo ""
+	echo "This script assumes that there is a single display connected."
+	exit 0
+fi
+CURRENT_OUTPUT=`xrandr -q | grep " connected" | cut -d" " -f1`
+CURRENT_MODE=`xvidtune -show | cut -d" " -f1 | tr -d \"`
+if [ $1 = "off" ] ; then
+	xrandr --output $CURRENT_OUTPUT --rate 60 --mode $CURRENT_MODE --fb $CURRENT_MODE --panning $CURRENT_MODE
+else
+	xrandr --output $CURRENT_OUTPUT --rate 60 --mode $CURRENT_MODE --fb $1 --panning $1
+fi
