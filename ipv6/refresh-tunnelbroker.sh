f4a66782f60e5122de11594bb10a1e1ccc5e9161
Hans-Peter Bergner <hapebe@gmx.de>
2025-03-23 16:00:47 +0100

    DynV6.com helpers

--- ipv6/refresh-tunnelbroker.sh
+++ ipv6/refresh-tunnelbroker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# as of approx. 2019:
 source ./.cred
 URL=https://$USER:$PASSWD@ipv4.tunnelbroker.net/nic/update?hostname=$HOSTNAME
 # echo $URL
---
+++ ipv6/update-dynv6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# 2025-03-23 for RaspberryPi Tina via LAN:
+source .env
+./dynv6.sh hapebe.dynv6.net eth0
