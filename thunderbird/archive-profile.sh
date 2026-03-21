d2de1febea2aeb7a2db9fffd825b4bedbb0e1870
Hans-Peter Bergner <hapebe@gmx.de>
2026-03-21 20:32:08 +0100

    Thunderbird profile restore verified

--- thunderbird/archive-profile.sh
+++ thunderbird/archive-profile.sh
@@ -3,3 +3,9 @@ tar -cJvf Thunderbird-$(date +%Y-%m-%d).tar.xz -C ~/.thunderbird pgbzgjtm.defaul
 # restore such backup:
 # cd ~/.thunderbird/
 # tar -xJvf /path/to/your/backup/Thunderbird-*.tar.xz
+#
+# ... and move / rename the profile folder to what was previously present on the target Thunderbird install:
+#
+# mv pgbzgjtm.default-esr co3d0q5u.default-esr
+#
+# Basically verified between two Fedora machines, 2026-03-21.
